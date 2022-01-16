4824c33651948dcaff2e6215b6ead197cd264599
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-01-16 23:41:30 +0100

    feat: First proof of concept of background styles

---
+++ src/shared/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className="navbar__container mt-4">
+            <h1 className="navbar__title"> Easy Route </h1>
+        </div>
+    )
+}
